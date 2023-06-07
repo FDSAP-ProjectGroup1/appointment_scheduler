46584bab1358712438d96fb8c142695433391b14
FDSAP-RUSTY <fdsap-rusty@FDSAP-Intern2.local>
2023-06-07 16:48:27 +0800

    new commit

--- lib/screens/Welcome/Login/loginScreen.dart
+++ lib/screens/Welcome/Login/loginScreen.dart
@@ -52,126 +52,135 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                 ),
-                Column(children: [
-                  LoginImage(),
-                ]),
-                Container(
-                  margin: EdgeInsets.all(0),
-                  padding: EdgeInsets.all(0),
-                  width: MediaQuery.of(context).size.width,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Color(0x00000000),
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.zero,
-                  ),
+                Expanded(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
                     children: [
-                      TextField(
-                        controller: emailController,
-                        obscureText: false,
-                        textAlign: TextAlign.start,
-                        maxLines: 1,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                        decoration: InputDecoration(
-                          disabledBorder: OutlineInputBorder(
+                      LoginImage(),
+                      SizedBox(height: 16),
+                      SingleChildScrollView(
+                        child: Container(
+                          margin: EdgeInsets.all(0),
+                          padding: EdgeInsets.all(0),
+                          width: MediaQuery.of(context).size.width,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: Color(0x00000000),
+                            shape: BoxShape.rectangle,
                             borderRadius: BorderRadius.zero,
-                            borderSide:
-                                BorderSide(color: Color(0xff000000), width: 1),
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero,
-                            borderSide:
-                                BorderSide(color: Color(0xff000000), width: 1),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              TextField(
+                                controller: emailController,
+                                obscureText: false,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                                decoration: InputDecoration(
+                                  disabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    borderSide: BorderSide(
+                                        color: Color(0xff000000), width: 1),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    borderSide: BorderSide(
+                                        color: Color(0xff000000), width: 1),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    borderSide: BorderSide(
+                                        color: Color(0xff000000), width: 1),
+                                  ),
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 14,
+                                    color: Color(0xff000000),
+                                  ),
+                                  filled: true,
+                                  fillColor: Color(0xffffffff),
+                                  isDense: false,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 8, horizontal: 12),
+                                ),
+                              ),
+                              TextField(
+                                controller: passwordController,
+                                obscureText: true,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                                decoration: InputDecoration(
+                                  disabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    borderSide: BorderSide(
+                                        color: Color(0xff000000), width: 1),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    borderSide: BorderSide(
+                                        color: Color(0xff000000), width: 1),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    borderSide: BorderSide(
+                                        color: Color(0xff000000), width: 1),
+                                  ),
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 14,
+                                    color: Color(0xff000000),
+                                  ),
+                                  filled: true,
+                                  fillColor: Color(0xffffffff),
+                                  isDense: false,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 8, horizontal: 12),
+                                ),
+                              ),
+                              MaterialButton(
+                                onPressed: () => loginProvider.login(
+                                    context,
+                                    emailController.text,
+                                    passwordController.text),
+                                color: Color(0xff000000),
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                                padding: EdgeInsets.all(16),
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    fontStyle: FontStyle.normal,
+                                  ),
+                                ),
+                                textColor: Color(0xffffffff),
+                                height: 50,
+                                minWidth: MediaQuery.of(context).size.width,
+                              ),
+                            ],
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero,
-                            borderSide:
-                                BorderSide(color: Color(0xff000000), width: 1),
-                          ),
-                          hintText: "Email",
-                          hintStyle: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 14,
-                            color: Color(0xff000000),
-                          ),
-                          filled: true,
-                          fillColor: Color(0xffffffff),
-                          isDense: false,
-                          contentPadding:
-                              EdgeInsets.symmetric(vertical: 8, horizontal: 12),
                         ),
-                      ),
-                      TextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        textAlign: TextAlign.start,
-                        maxLines: 1,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                        decoration: InputDecoration(
-                          disabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero,
-                            borderSide:
-                                BorderSide(color: Color(0xff000000), width: 1),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero,
-                            borderSide:
-                                BorderSide(color: Color(0xff000000), width: 1),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero,
-                            borderSide:
-                                BorderSide(color: Color(0xff000000), width: 1),
-                          ),
-                          hintText: "Password",
-                          hintStyle: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 14,
-                            color: Color(0xff000000),
-                          ),
-                          filled: true,
-                          fillColor: Color(0xffffffff),
-                          isDense: false,
-                          contentPadding:
-                              EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                        ),
-                      ),
-                      MaterialButton(
-                        onPressed: () => loginProvider.login(context,
-                            emailController.text, passwordController.text),
-                        color: Color(0xff000000),
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            fontStyle: FontStyle.normal,
-                          ),
-                        ),
-                        textColor: Color(0xffffffff),
-                        height: 50,
-                        minWidth: MediaQuery.of(context).size.width,
                       ),
                     ],
                   ),
